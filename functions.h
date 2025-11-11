49576f00c7d93b6f426a996d28a335c37193cff9
Hyna <bylo@duck.com>
2025-11-12 00:44:38 +0100

    ajout fichier d'entête

---
+++ functions.h
@@ -0,0 +1,11 @@
+/*
+* prototypes d'entêtes des fonctions 
+* sert à déclarer les fonctions
+*/ 
+#ifndef __MY_MATH_LIB_H
+#define __MY_MATH_LIB_H
+
+unsigned int fac(unsigned int value);
+unsigned add(int counter, ...);
+
+#endif
