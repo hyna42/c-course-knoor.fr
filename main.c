e4e7787db05ca16b65f0fa87bcd33882ee95c33b
Hyna <bylo@duck.com>
2025-11-12 00:17:56 +0100

    fonction avec nombre d'arguments variables

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 unsigned int fac(unsigned int value)
 {
@@ -14,6 +15,23 @@ unsigned int fac(unsigned int value)
     return result;
 }
 
+int add(int counter, ...)
+{
+    int sum = 0;
+
+    va_list paramInfos;
+
+    va_start(paramInfos, counter);
+
+    while (counter > 0)
+    {
+        sum += va_arg(paramInfos, int);
+        counter--;
+    }
+    va_end(paramInfos);
+    return sum;
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -24,5 +42,10 @@ int main(int argc, char *argv[])
     {
         printf("Argument at position %2d == %s\n", i, argv[i]);
     }
+
+    puts("********* funciton with arg variables **********");
+
+    printf("somme : %d\n", add(3, 7, 1, 2));
+
     return 0;
 }
