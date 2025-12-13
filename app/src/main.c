651063042c4892e03ec72f8c2c51b33970e93386
hyna42 <bylo@duck.com>
2025-12-13 21:28:38 +0100

    funciton update

--- app/src/main.c
+++ app/src/main.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 // #include <stddef.h>
 // #include <stdlib.h>
-// #include <stdarg.h>
+#include <stdarg.h>
 #include "../libs/add/addition.h"
 #include "../libs/fact/factoriel.h"
 
-
-int main(int argc, char * argv [] )
+int main(int argc, char *argv[])
 {
 
+    puts("############ FONCTIONS ############");
 
-    puts("**** utilisation des arguments passées à une fonction ****");
-    for (int i = 0; i < argc;i++){
-        //cettte fonction affiche les différents arguments passés à la fonction
+    puts("**** calcul factoriel ****");
+    printf("factoriel de 5 = %d \n", factoriel(5));
+    printf("factoriel de 4 = %d \n", factoriel(4));
+
+    // 1. fonction avec arguments
+    puts("**** 1. Fontions avec arguments **** ****");
+    for (int i = 1; i < argc; i++)
+    {
+        // cettte fonction affiche les différents arguments passés à la fonction
         printf("Argument à l'index %2d == %s\n", i, argv[i]);
     }
 
-        puts("**** calcul factoriel ****");
-    printf("factoriel de 5 = %d \n", factoriel(5));
-    printf("factoriel de 4 = %d \n", factoriel(4));
-    
-    
+    // fonction avec arguments
+    puts("**** 2. Fontions avec arguments variable **** ****");
     puts("**** somme dynamique****");
     printf("somme (3,7,8,9) = %d\n", addition(3, 7, 8, 9));
 
