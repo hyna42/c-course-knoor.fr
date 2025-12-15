e0ee8cf79b7b30e8e9bc4f136a1d121b2756b52d
hyna42 <bylo@duck.com>
2025-12-15 23:42:12 +0100

    exo pointeur

--- app/src/main.c
+++ app/src/main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap(int *a, int *b)
+{
+    int temVar = *a;
+    *a = *b;
+    *b = temVar;
+
+    printf("adresse a = %p, adress b = %p\n", (void *)a, (void *)b);
+    printf("valeur a = %d, valeur b = %d\n", *a,*b);
+}
 
 int main(void)
 {
     puts("############ MANIPULATION DE LISTES CHAINEES ############");
 
-   
+    int a = 10;
+    int b = 20;
+    swap(&a, &b);
 
     return EXIT_SUCCESS;
 }
